79185692fc36419741f7398580ff0e218dd7f27a
Abhijit Gupta <avgupta456@gmail.com>
2023-03-04 21:23:36 -0500

    fix scripts

--- baseline_main.py
+++ baseline_main.py
@@ -1,49 +1,43 @@
 import argparse
-import pickle
 
 import numpy as np
 import torch
 from torch_geometric.data import DataLoader
 from tqdm import tqdm
 
-from explainers import *
-from gnns import *
+from explainers import CF_Explainer, CXPlain, GNNExplainer, PGExplainer, PGMExplainer
 from utils.dataset import get_datasets
 
-feature_dict = {"BA_Community": 10, "BA_shapes": 10, "Tree_Cycle": 10, "Tree_Grids": 10,"cornell": 1703, "cora":1433,
-                "mutag": 14, "ba3": 4, "mnist": 1, "tox21": 9, "reddit": 1, "bbbp": 9, "NCI1": 37}
-task_type = {"BA_Community": "nc", "BA_shapes": "nc", "Tree_Cycle": "nc", "Tree_Grids":"nc","cornell": "nc", "cora":"nc",
-                "mutag": "gc", "ba3": "gc", "mnist": "gc", "tox21": "gc", "reddit": "gc", "bbbp": "gc", "NCI1": "gc"}
+from constants import task_type, feature_dict, add_dataset_args, add_explainer_args
+
+Explainer = PGExplainer | PGMExplainer | GNNExplainer | CXPlain | CF_Explainer
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Train explainers")
-    parser.add_argument('--cuda', type=int, default=5,
-                        help='GPU device.')
-    parser.add_argument('--root', type=str, default="results/",
-                        help='Result directory.')
-    parser.add_argument('--dataset', type=str, default='Tree_Cycle',
-                        choices=["BA_shapes", "Tree_Cycle", "Tree_Grids", "cornell"
-                                 'mutag', 'ba3', "bbbp", "NCI1"])
-    parser.add_argument('--explainer', type=str, default="PGExplainer",
-                        choices=['GNNExplainer', 'PGExplainer', 'PGMExplainer',
-                                 'CXPlain', 'CF_Explainer'])
+    parser.add_argument("--cuda", type=int, default=5, help="GPU device.")
+    parser.add_argument(
+        "--root", type=str, default="results/", help="Result directory."
+    )
+    parser = add_dataset_args(parser)
+    parser = add_explainer_args(parser)
+
     # gflow explainer related parametersn
-    parser.add_argument('--gnn_type', type=str, default="gcn")
-    parser.add_argument('--task', type=str, default="nc")
-    parser.add_argument('--verbose', type=int, default=10)
-    parser.add_argument('--train_batchsize', type=int, default=16)            ####
-    parser.add_argument('--test_batchsize', type=int, default=16)            ####
-    parser.add_argument('--epoch', type=int, default=3000)
-    parser.add_argument('--feature_in', type=int)
-    parser.add_argument('--n_hidden', type=int, default=128)              ####
-    parser.add_argument('--num_test', type=int, default=50)
+    parser.add_argument("--gnn_type", type=str, default="gcn")
+    parser.add_argument("--task", type=str, default="nc")
+    parser.add_argument("--verbose", type=int, default=10)
+    parser.add_argument("--train_batchsize", type=int, default=16)
+    parser.add_argument("--test_batchsize", type=int, default=16)
+    parser.add_argument("--epoch", type=int, default=3000)
+    parser.add_argument("--feature_in", type=int)
+    parser.add_argument("--n_hidden", type=int, default=128)
+    parser.add_argument("--num_test", type=int, default=50)
 
-
-    parser.add_argument('--dropout', type=float, default=0.001)
-    parser.add_argument('--learning_rate', type=float, default=1e-3)    ####
-    parser.add_argument('--lr_decay', type=float, default=0.999)
-    parser.add_argument('--weight_decay', type=float, default=0)
-    parser.add_argument('--if_cf', type=bool, default=True)
+    parser.add_argument("--dropout", type=float, default=0.001)
+    parser.add_argument("--learning_rate", type=float, default=1e-3)
+    parser.add_argument("--lr_decay", type=float, default=0.999)
+    parser.add_argument("--weight_decay", type=float, default=0)
+    parser.add_argument("--if_cf", type=bool, default=True)
 
     return parser.parse_args()
 
@@ -52,65 +46,90 @@ args = parse_args()
 args.device = torch.device(f"cuda:{args.cuda}" if torch.cuda.is_available() else "cpu")
 mr = [0, 0.02, 0.04, 0.06, 0.08, 0.1, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5]
 results = {}
-for dataset in ["BA_shapes", "Tree_Cycle", "Tree_Grids", "cornell", 'mutag', 'ba3', "bbbp", "NCI1"]:
+for dataset in [
+    "BA_shapes",
+    "Tree_Cycle",
+    "Tree_Grids",
+    "cornell",
+    "mutag",
+    "ba3",
+    "bbbp",
+    "NCI1",
+]:
     data_wise_resutls = {}
     args.dataset = dataset
     args.feature_in = feature_dict[args.dataset]
     args.task = task_type[args.dataset]
     train_dataset, val_dataset, test_dataset = get_datasets(name=args.dataset)
-    test_loader = DataLoader(test_dataset[:args.num_test], batch_size=1, shuffle=False, drop_last=False)
-    gnn_path = f'param/gnns/{args.dataset}_{args.gnn_type}.pt'
-    for ex in ['PGExplainer', 'CF_Explainer', 'GNNExplainer', 'PGMExplainer', 'CXPlain']:
+    test_loader = DataLoader(
+        test_dataset[: args.num_test], batch_size=1, shuffle=False, drop_last=False
+    )
+    gnn_path = f"param/gnns/{args.dataset}_{args.gnn_type}.pt"
+    for ex in [
+        "PGExplainer",
+        "CF_Explainer",
+        "GNNExplainer",
+        "PGMExplainer",
+        "CXPlain",
+    ]:
         data_wise_resutls[ex] = []
         args.explainer = ex
         if args.explainer in ["PGExplainer"]:
-            exec(f"Explainer = {args.explainer}(args.device, gnn_path, task=args.task, n_in_channels=args.feature_in)")
+            Explainer = eval(args.explainer)(
+                args.device, gnn_path, task=args.task, n_in_channels=args.feature_in
+            )
         else:
-            exec(f"Explainer = {args.explainer}(args.device, gnn_path, task=args.task)")
+            Explainer = eval(args.explainer)(args.device, gnn_path, task=args.task)
         acc_logger, fid_logger = [], []
         # for graph in tqdm(iter(test_loader), total=len(test_loader)):
         for graph in tqdm(iter(test_loader), total=len(test_loader)):
             graph.to(args.device)
             edge_imp = Explainer.explain_graph(graph)
-            acc, fidelity = Explainer.evaluate_acc(mr, graph=graph, imp=edge_imp, if_cf=True)
+            acc, fidelity = Explainer.evaluate_acc(
+                mr, graph=graph, imp=edge_imp, if_cf=True
+            )
             acc_logger.append(acc)
             fid_logger.append(fidelity)
-        S_A = np.mean(acc_logger,axis=0, keepdims=False)[0]
-        S_F = np.mean(fid_logger,axis=0, keepdims=False)[0]
+        S_A = np.mean(acc_logger, axis=0, keepdims=False)[0]
+        S_F = np.mean(fid_logger, axis=0, keepdims=False)[0]
         auc = np.array(acc_logger).mean(axis=0).mean()
         data_wise_resutls[ex].append(S_A)
         data_wise_resutls[ex].append(S_F)
         data_wise_resutls[ex].append(auc)
         print(f"--------{dataset} with {ex}----------")
-        print("Sparsity V.S. Acc:", np.mean(acc_logger,axis=0, keepdims=False)[0])
-        print('AUC: %.3f' % np.array(acc_logger).mean(axis=0).mean())
-        print("Sparsity V.S. Fidelity:", np.mean(fid_logger,axis=0, keepdims=False)[0])
-        print('AUC-fid: %.3f' % np.array(fid_logger).mean(axis=0).mean())
+        print("Sparsity V.S. Acc:", np.mean(acc_logger, axis=0, keepdims=False)[0])
+        print("AUC: %.3f" % np.array(acc_logger).mean(axis=0).mean())
+        print("Sparsity V.S. Fidelity:", np.mean(fid_logger, axis=0, keepdims=False)[0])
+        print("AUC-fid: %.3f" % np.array(fid_logger).mean(axis=0).mean())
     results[dataset] = data_wise_resutls
 
-    gnn_path = f'param/gnns/{args.dataset}_{args.gnn_type}_attr.pt'
-    for ex in ['SAExplainer', 'GradCam', 'IGExplainer', "RandomCaster"]:
+    gnn_path = f"param/gnns/{args.dataset}_{args.gnn_type}_attr.pt"
+    for ex in ["SAExplainer", "GradCam", "IGExplainer", "RandomCaster"]:
         data_wise_resutls[ex] = []
         args.explainer = ex
-        exec(f"Explainer = {args.explainer}(args.device, gnn_path, task=args.task, ds=args.dataset)")
+        Explainer = eval(args.explainer)(
+            args.device, gnn_path, task=args.task, ds=args.dataset
+        )
+
         acc_logger, fid_logger = [], []
         # for graph in tqdm(iter(test_loader), total=len(test_loader)):
         for graph in tqdm(iter(test_loader), total=len(test_loader)):
             graph.to(args.device)
             edge_imp = Explainer.explain_graph(graph)
-            acc, fidelity = Explainer.evaluate_acc(mr, graph=graph, imp=edge_imp, if_cf=True)
+            acc, fidelity = Explainer.evaluate_acc(
+                mr, graph=graph, imp=edge_imp, if_cf=True
+            )
             acc_logger.append(acc)
             fid_logger.append(fidelity)
-        S_A = np.mean(acc_logger,axis=0, keepdims=False)[0]
-        S_F = np.mean(fid_logger,axis=0, keepdims=False)[0]
+        S_A = np.mean(acc_logger, axis=0, keepdims=False)[0]
+        S_F = np.mean(fid_logger, axis=0, keepdims=False)[0]
         auc = np.array(acc_logger).mean(axis=0).mean()
         data_wise_resutls[ex].append(S_A)
         data_wise_resutls[ex].append(S_F)
         data_wise_resutls[ex].append(auc)
         print(f"--------{dataset} with {ex}----------")
-        print("Sparsity V.S. Acc:", np.mean(acc_logger,axis=0, keepdims=False)[0])
-        print('AUC: %.3f' % np.array(acc_logger).mean(axis=0).mean())
-        print("Sparsity V.S. Fidelity:", np.mean(fid_logger,axis=0, keepdims=False)[0])
-        print('AUC-fid: %.3f' % np.array(fid_logger).mean(axis=0).mean())
+        print("Sparsity V.S. Acc:", np.mean(acc_logger, axis=0, keepdims=False)[0])
+        print("AUC: %.3f" % np.array(acc_logger).mean(axis=0).mean())
+        print("Sparsity V.S. Fidelity:", np.mean(fid_logger, axis=0, keepdims=False)[0])
+        print("AUC-fid: %.3f" % np.array(fid_logger).mean(axis=0).mean())
     results[dataset] = data_wise_resutls
-
